18ea26504eab5e778ad4573228efd96bf6459bb5
lunawang0104 <60398978+lunawang0104@users.noreply.github.com>
2020-01-28 11:38:04 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+//this set up fuction only gets run once
+function setup() {
+  createCanvas(400, 400);
+}
+
+//will continuelly run
+function draw() {
+  background(300);
+  //(x,y,with,height)
+  rect(0,0,100,50);
+  rect(100,0,150,50);
+  rect(250,0,150,150);
+  rect(0,50,50,100);
+  rect(50,50,200,200);
+  rect(250,150,75,100);
+  rect(325,150,75,100);
+  rect(0,150,50,150);
+  rect(0,300,50,100);
+  rect(50,250,100,100);
+  rect(150,250,100,50);
+  rect(150,300,100,50);
+  rect(250,250,150,50);
+  rect(150,350,100,25);
+  rect(250,250,150,125);
+  
+}
